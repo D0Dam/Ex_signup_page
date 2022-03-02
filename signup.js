f5129c8c03afdedc6f2d02a7384e54e44688e0ad
KimMinJae <smallkdb@gmail.com>
2022-03-03 02:14:40 +0900

    add signup tool finish

--- signup.js
+++ signup.js
@@ -1,5 +1,5 @@
 function changePhone1(){
-    const phone1 = document.getElementById("phone1").value
+    const phone1 = document.getElementById("phone1").value // 010
     if(phone1.length === 3){
         document.getElementById("phone2").focus();
     }
@@ -56,10 +56,10 @@ const getToken = () => {
 };
 
 function checkCompletion(){
-    alert("문자 인증이 완료되었습니다.")
+    setTimeout(function() { alert("문자 인증이 완료되었습니다.") }, 10);
     initButton();
-    document.getElementById("completion").innerHTML = "인증완료"
-    document.getElementById("signUpButton").disabled = flase;
+    document.getElementById("completion").innerHTML = "인증완료";
+    document.getElementById("signUpButton").disabled = false;
     document.getElementById("signUpButton").setAttribute("style","background-color:yellow;")
 }
 
@@ -69,7 +69,86 @@ function signUpCheck(){
     let email = document.getElementById("email").value
     let name = document.getElementById("name").value
     let password = document.getElementById("password").value
-    let passwordCheck = document.getElementById("password").value
+    let passwordCheck = document.getElementById("passwordCheck").value
     let area = document.getElementById("area").value
-    let email = document.getElementById("gender_man").checked
+    let gender_man = document.getElementById("gender_man").checked
+    let gender_woman = document.getElementById("gender_woman").checked
+    let check = true;
+
+    if(email.includes('@')){
+        let emailId = email.split('@')[0]
+        let emailServer = email.split('@')[1]
+        if(emailId === "" || emailServer === ""){
+            document.getElementById("emailError").innerHTML = "이메일이 올바르지 않습니다."
+            check = false
+        }
+        else{
+            document.getElementById("emailError").innerHTML = ""
+        }
+    }
+     else {
+        document.getElementById("emailError").innerHTML = "이메일이 올바르지 않습니다."
+        check = false
+    }
+    
+    if(name===""){
+        document.getElementById("nameError").innerHTML="이름이 올바르지 않습니다."
+        check = false
+    }
+    else{
+        document.getElementById("nameError").innerHTML=""
+    }
+
+    if(password != passwordCheck){
+        document.getElementById("passwordError").innerHTML=""
+        document.getElementById("passwordCheckError").innerHTML="비밀번호가 동일하지 않습니다."
+        check = false
+      }else{
+        document.getElementById("passwordError").innerHTML=""
+        document.getElementById("passwordCheckError").innerHTML=""
+      }
+    
+      if(password===""){
+        document.getElementById("passwordError").innerHTML="비밀번호를 입력해주세요."
+        check = false
+      }else{
+        //document.getElementById("passwordError").innerHTML=""
+      }
+      if(passwordCheck===""){
+        document.getElementById("passwordCheckError").innerHTML="비밀번호를 다시 입력해주세요."
+        check = false
+      }else{
+        //document.getElementById("passwordCheckError").innerHTML=""
+      }
+    
+    
+      // 지역선택 확인
+      if(area === "지역을 선택하세요."){
+        document.getElementById("areaError").innerHTML="지역을 선택해주세요."
+        check = false
+      }else{
+        document.getElementById("areaError").innerHTML=""
+      }
+    
+      // 성별체크확인
+      if(!gender_man && !gender_woman){
+        document.getElementById("genderError").innerHTML="성별을 선택해주세요."
+        check = false
+      }else{
+        document.getElementById("genderError").innerHTML=""
+      }
+      
+      if(check){
+        document.getElementById("emailError").innerHTML=""
+        document.getElementById("nameError").innerHTML=""
+        document.getElementById("passwordError").innerHTML=""
+        document.getElementById("passwordCheckError").innerHTML=""
+        document.getElementById("areaError").innerHTML=""
+        document.getElementById("genderError").innerHTML=""
+        
+        //비동기 처리이벤트
+        setTimeout(function() {
+          alert("가입이 완료되었습니다.")
+      },0);
+    }
 }
